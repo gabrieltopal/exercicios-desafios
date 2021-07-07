081629a34cfee5aa2cb7d82a61517a13ea745fe3
Gabriel Topal <gabriel.topal26@gmail.com>
2021-07-07 15:28:52 -0400

    create ambiente.js aula13 + desafio10 html/css changes

---
+++ estudos/javascript/aulas/aula13/ambiente.js
@@ -0,0 +1,22 @@
+console.log('Vai começar...')
+for (var c = 1; c <= 10; c++) {
+    console.log(`Passo ${c}`)
+}
+console.log('Fim!')
+
+/*
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
+*/
+
+/*
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
